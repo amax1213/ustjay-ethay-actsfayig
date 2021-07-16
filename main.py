881791bfaf8229bd915f25115425fa1c6920943f
allen maxwell <allen.maxwell@outlook.com>
2021-07-16 15:26:41 -0700

    Initial commit

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
 def get_fact():
-
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-
     return facts[0].getText()
 
+def get_pig_latin(fact):
+    payload = {"input_text": fact}
+    response = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload)
+    return response
+    
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    fact = get_fact().strip()
+    body = get_pig_latin(fact)
+    return Response(response=body.url, mimetype="text/html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) 
 
